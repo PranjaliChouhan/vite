da1df72705e753ee6bc98ba5507492b7a01fbeaf
PranjaliChouhan <pranjalichouhan4@gmail.com>
2023-07-05 01:00:17 +0530

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+// Select the container element
+const headingContainer = document.getElementById('headingContainer');
+
+// Create a new heading element
+const heading = document.createElement('h1');
+
+// Set the text content of the heading
+heading.textContent = 'Hello javascript with vite';
+
+// Append the heading to the container
+headingContainer.appendChild(heading);
